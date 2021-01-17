86954a38cc07c2a85d8d04ce3849ae23c8ac47f2
Jeahoon Jung <jhoon5245@gmail.com>
2021-01-18 08:08:28 +1300

    #15.0 Local Only Fields

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,3 +1,15 @@
+import { useReactiveVar } from "@apollo/client";
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedOutRouter = () => <h2>Logged In</h2>;
+export const LoggedInRouter = () => {
+  const onClick = () => {
+    isLoggedInVar(false);
+  };
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={onClick}>Click to log out</button>
+    </div>
+  );
+};
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,15 @@
 import React from "react";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedOutRouter = () => <h2>Logged Out</h2>;
+export const LoggedOutRouter = () => {
+  const onClick = () => {
+    isLoggedInVar(true);
+  };
+
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <button onClick={onClick}>Click to login</button>
+    </div>
+  );
+};
