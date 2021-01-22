3c13ff3bb7088168ea52cba0a417c2493b31334d
Jeahoon Jung <jhoon5245@gmail.com>
2021-01-22 22:56:51 +1300

    #17.2 Restaurants List

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -53,16 +53,30 @@ export const Restaurants = () => {
         />
       </form>
       {!loading && (
-        <div className="max-w-screen-xl mx-auto mt-8">
+        <div className="max-w-screen-xl mx-auto mt-8 px-5 xl:px-0">
           <div className="flex justify-center mx-auto overflow-x-auto">
             {data?.allCategories.categories?.map((category) => (
-              <div className="mx-4 lg:mx-8">
+              <div className="flex flex-col items-center mx-4 lg:mx-8 group">
                 <div
-                  className="w-14 h-14 rounded-full bg-cover hover:bg-gray-200 cursor-pointer"
+                  className="w-14 h-14 rounded-full bg-cover group-hover:bg-gray-200 cursor-pointer"
                   style={{ backgroundImage: `url(${category.coverImg})` }}
                 ></div>
-                <span className="mt-1 text-sm flex flex-col items-center font-medium">
+                <span className="mt-1 text-sm flex flex-col items-center font-medium cursor-pointer">
                   {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className="grid mt-10 grid-cols-3 gap-x-5 gap-y-10">
+            {data?.restaurants.results?.map((restaurant) => (
+              <div>
+                <div
+                  className="bg-red-500 py-28 bg-cover bg-center mb-3"
+                  style={{ backgroundImage: `url(${restaurant.coverImg})` }}
+                ></div>
+                <h3 className="text-md font-semibold">{restaurant.name}</h3>
+                <span className="border-t-2 border-gray-200">
+                  {restaurant.category?.name}
                 </span>
               </div>
             ))}
