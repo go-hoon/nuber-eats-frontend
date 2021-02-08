30f7d2d75b11a652794993de2626f4442cd1abed
Jeahoon Jung <jhoon5245@gmail.com>
2021-02-08 21:35:17 +1300

    #19.7 EditProfile E2E part Two

--- cypress/integration/user/edit-profile.ts
+++ cypress/integration/user/edit-profile.ts
@@ -11,6 +11,12 @@ describe("Edit Profile", () => {
     user.window().title().should("eq", "Edit Profile | Nuber Eats");
   });
   it("can change email", () => {
+    user.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      if (req.body.operationName === "editProfile") {
+        // @ts-ignore
+        req.body?.variables?.input?.email = "test@test.com";
+      }
+    });
     user.get('a[href="/edit-profile"]').click();
     user.findByPlaceholderText(/email/i).clear().type("new@test.com");
     user.findByRole("button").click();
