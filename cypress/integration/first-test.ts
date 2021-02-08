8440b50c0500a819129c3e93f5c233c3a5120f75
Jeahoon Jung <jhoon5245@gmail.com>
2021-02-08 15:45:00 +1300

    #19.2 Login E2E

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,29 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Nuber Eats");
+describe("Log In", () => {
+  const user = cy;
+  it("should see login page", () => {
+    user.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+  it("can see email / password validation errors", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("test@test");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("test@test.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("1")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("test@test.com");
+    user.findByPlaceholderText(/password/i).type("12345");
+    user
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click();
+    user.window().its("localStorage.nuber-token").should("be.a", "string");
   });
 });
