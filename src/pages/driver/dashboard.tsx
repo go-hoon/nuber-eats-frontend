0a4869e0182ae5d89941ee2c8ae9d89070ded2f3
Jeahoon Jung <jhoon5245@gmail.com>
2021-02-20 18:45:31 +1300

    #23.10 Coocked Order Subscription

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 import GoogleMapReact from "google-map-react";
+import { gql, useSubscription } from "@apollo/client";
+import { FULL_ORDER_FRAGMENT } from "../../fragments";
+import { cookedOrders } from "../../__generated/cookedOrders";
+import { Link } from "react-router-dom";
+
+const COOKED_ORDERS_SUBSCRIPTION = gql`
+  subscription cookedOrders {
+    cookedOrders {
+      ...FullOrderParts
+    }
+  }
+  ${FULL_ORDER_FRAGMENT}
+`;
 
 interface ICoords {
   lat: number;
@@ -16,6 +29,15 @@ const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚖</div>
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
   const [map, setMap] = useState<google.maps.Map>();
+  const { data: cookedOrderData } = useSubscription<cookedOrders>(
+    COOKED_ORDERS_SUBSCRIPTION
+  );
+
+  useEffect(() => {
+    if (cookedOrderData?.cookedOrders.id) {
+      makeRoute();
+    }
+  }, [cookedOrderData]);
 
   // @ts-ignore
   const onSucces = ({ coords: { latitude, longitude } }: Position) => {
@@ -48,7 +70,7 @@ export const Dashboard = () => {
     map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
   };
-  const onGetRouteClick = () => {
+  const makeRoute = () => {
     if (map) {
       const directionsService = new google.maps.DirectionsService();
       const directionsRenderer = new google.maps.DirectionsRenderer();
@@ -96,9 +118,24 @@ export const Dashboard = () => {
           {/* <Driver lat={driverCoords.lat} lng={driverCoords.lng} /> */}
         </GoogleMapReact>
       </div>
-      <button className="btn" onClick={onGetRouteClick}>
-        Get Route
-      </button>
+      <div className="max-w-screen-sm mx-auto bg-white relative -top-10 shadow-lg text-xl font-medium px-5 py-8">
+        {cookedOrderData?.cookedOrders ? (
+          <>
+            <h1 className="text-center my-3 text-3xl">New Cooked Order</h1>
+            <h4 className="text-center my-3 text-2xl">
+              Pick it up soon @ {cookedOrderData?.cookedOrders.restaurant?.name}
+            </h4>
+            <Link
+              to={`/orders/${cookedOrderData.cookedOrders.id}`}
+              className="btn w-full mt-5 block text-center"
+            >
+              Accept Chanllenge &rarr;
+            </Link>
+          </>
+        ) : (
+          <h1 className="text-center my-3 text-3xl">No orders yet!</h1>
+        )}
+      </div>
     </div>
   );
 };
