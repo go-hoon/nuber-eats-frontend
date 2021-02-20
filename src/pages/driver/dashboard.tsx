ad66f676cb322dffdf5a4ef19cb3cd85e41cc436
Jeahoon Jung <jhoon5245@gmail.com>
2021-02-20 17:38:06 +1300

    #23.8 Address Geocoding

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -6,10 +6,17 @@ interface ICoords {
   lng: number;
 }
 
+interface IDriverProps {
+  lat: number;
+  lng: number;
+  $hover?: any;
+}
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚖</div>;
+
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
-  const [map, setMap] = useState<any>();
-  const [maps, setMaps] = useState<any>();
+  const [map, setMap] = useState<google.maps.Map>();
+
   // @ts-ignore
   const onSucces = ({ coords: { latitude, longitude } }: Position) => {
     setDriverCoords({ lat: latitude, lng: longitude });
@@ -24,14 +31,22 @@ export const Dashboard = () => {
     });
   }, []);
   useEffect(() => {
-    if (map && maps) {
-      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    if (map) {
+      map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+      //   const geocoder = new google.maps.Geocoder();
+      //   geocoder.geocode(
+      //     {
+      //       location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+      //     },
+      //     (results, status) => {
+      //       console.log(status, results);
+      //     }
+      //   );
     }
   }, [driverCoords.lat, driverCoords.lng]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-    map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
     setMap(map);
-    setMaps(maps);
   };
   return (
     <div>
@@ -50,14 +65,7 @@ export const Dashboard = () => {
           }}
           bootstrapURLKeys={{ key: "AIzaSyDERrmnPNGJyjFrN1R-0cNEAk3JwxLW4lc" }}
         >
-          <div
-            // @ts-ignore
-            lat={driverCoords.lat}
-            lng={driverCoords.lng}
-            className="text-lg"
-          >
-            🚖
-          </div>
+          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
         </GoogleMapReact>
       </div>
     </div>
